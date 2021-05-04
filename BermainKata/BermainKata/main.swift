18c21be7ce7681744327a30b0e9b71e4ef5e8e0a
suryaharahap <suryaharahap18@gmail.com>
2021-05-04 16:37:03 +0700

    Add Codelabs

---
+++ BermainKata/BermainKata/main.swift
@@ -0,0 +1,83 @@
+//
+//  main.swift
+//  BermainKata
+//
+//  Created by Surya on 03/05/21.
+//
+
+import Foundation
+
+print("Hello, mari bermain kata!")
+print("--------------------")
+
+// Mengambil inputan
+print("Masukan nama depan anda:"); let firstName = String(readLine() ?? "")
+print("Masukan nama belakang anda:"); let lastName = String(readLine() ?? "")
+print("---------------------")
+print("Berikut ini adalah operator-operator yang digunakan:")
+
+// Operator Concatenation
+let concatenationString = firstName + lastName
+print("1. String Concatenation Operator : \"\(concatenationString)\"")
+
+
+//Operator Mutability
+var mutabilityString  = ""
+mutabilityString += firstName
+mutabilityString += lastName
+print("2. String mutability Operator: \"\(mutabilityString)\"")
+
+//Operator Comparison
+var comparisonString = ""
+if firstName == lastName {
+    comparisonString = String(true)
+} else {
+    comparisonString = String(false)
+}
+
+print("3. String Comparison Operator : \"\(comparisonString)\"")
+print("----------------------------------")
+print("Berikut ini adalah fungsi-fungsi yang digunakan:")
+
+//Menggabungkan nama depan dan belakang
+let fullName = firstName + " " + lastName
+
+// Fungsi isEmpty
+let empty = firstName.isEmpty || lastName.isEmpty
+print("1. Fungsi isEmpty: \"\(empty)\"")
+
+// Fungsi startIndex
+let startIndex = fullName[fullName.startIndex]
+print("2. Fungsi startIndex : \"\(startIndex)\"")
+
+// Fungsi index
+let index = fullName[fullName.index(after: fullName.startIndex)]
+print("3. Fungsi index : \"\(index)\"")
+
+// Fungsi index Lainnya
+let customIndex = fullName[fullName.index(fullName.startIndex, offsetBy: 5)]
+print("4. Index ke lima : \"\(customIndex)\"")
+
+// Fungsi endIndex
+let endIndex = fullName[fullName.index(before: fullName.endIndex)]
+print("5. Fungsi endIndex : \"\(endIndex)\"")
+
+// Fungsi insert
+var insert = fullName
+insert.insert("!", at:insert.endIndex)
+print("6. Fungsi insert : \"\(insert)\"")
+
+// Fungsi remove
+var remove = insert
+remove.remove(at: remove.index(before: remove.endIndex))
+print("7. Fungsi remove : \"\(remove)\"")
+
+// Fungsi append
+var append = firstName
+append.append(lastName)
+print("8. Fungsi append : \"\(append)\"")
+
+// Fungsi count
+let count = fullName.count
+print("9. Fungsi length : \(count)")
+print("----------------------------------")
